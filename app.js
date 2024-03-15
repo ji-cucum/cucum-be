8cd1568bb332901a183df42df14cc71011694d85
JEONG <dlfhfl63@gmail.com>
2024-03-16 00:36:23 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const { Pool } = require('pg');
+const app = express()
+app.use(bodyParser.json());
+const port = 3000
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+console.log(process.env.DB_PASSWORD)
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+    }
+)
+
+
+app.get('/data', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM ji_project.music');
+    res.send(result.rows);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
