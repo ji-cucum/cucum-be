622c6b325fd05022acb83708f5da428162282d7d
JEONG <dlfhfl63@gmail.com>
2024-03-18 13:06:32 +0900

    add contant register,login and Fixing login function error

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const pool = require('./db');
 const app = express()
 app.use(bodyParser.json());
 const port = 3000
+const pool = require('./db');
+const bcrypt = require("bcrypt");
+const session = require("express-session");
+const flash = require("express-flash");
+const passport = require("passport");
+
+const initializePassport = require('./passportConfig')
+
+initializePassport(passport)
 
 console.log(process.env.DB_PASSWORD)
 app.get('/', (req, res) => {
@@ -13,6 +21,21 @@ app.get('/', (req, res) => {
 );
 
 app.set("view engine" , "ejs");
+app.use(express.urlencoded({ extended: false})); 
+
+app.use(session({
+  secret: 'secret',
+
+  resave: false,
+
+  saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
 
 app.get("/users/register", (req,res) => {
   res.render("register");
@@ -22,8 +45,79 @@ app.get("/users/login", (req,res) => {
   res.render("login");
 });
 
+app.post("/users/register", async (req,res) => {
+  let {name, email, password, password2} = req.body;
+  console.log({
+    name,
+    email,
+    password,
+    password2
+  });
+
+  let errors = [];
+
+  if (!name || !email || !password || !password2){
+    errors.push({ massage: "Please enter all fields" });
+  }
+
+  if(password.length < 6){
+    errors.push({ message: "Password should be at least 6 characters" });
+  }
+
+  if(password !== password2){
+    errors.push({ message: "Password do not match" });
+  }
+
+  if(errors.length > 0){
+    res.render("register", { errors });
+  }else{
+    //入力情報がフォーム様式にしたかったことを確認した後
+
+    let hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+
+    pool.query(
+      `SELECT * FROM ji_project.users
+      WHERE email = $1`, 
+      [email], 
+      (err, results) => {
+        if(err){
+          throw err;
+        }
+        console.log(results.rows);
+
+        if(results.rows.length > 0){
+          errors.push({message: "Email already registered"})
+          res.render('register', { errors });
+        }else{
+          pool.query(
+            `INSERT INTO ji_project.users (name, email, password)
+            VALUES ($1, $2, $3)
+            RETURNING id, password`, [name, email, hashedPassword], 
+            (err, results) => {
+              if (err){
+                throw err;
+              }
+              console.log(results.row);
+              req.flash("success_msg", "You are now registered. Please log in");
+              res.redirect("/users/login")
+            }
+          )
+        }
+      }
+    )
+  }
+});
+
+app.post("/users/login", passport.authenticate('local',{
+  successRedirect: "/users/dashboard",
+  failureRedirect: "/users/login",
+  failureFlash: true
+  })
+);
+
 app.get("/users/dashboard", (req,res) => {
-  res.render("dashboard", { user:"Conor"});
+  res.render("dashboard", { user: req.user.name });
 });
 
 app.get('/data', async (req, res) => {
