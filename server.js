1f318eacd45704e3db88e563fc41bedf70a1c237
hangyuCho <chg3376@gmail.com>
2024-03-22 07:31:32 +0900

    fix : fix port number 3000 -> 3011 and add cors

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import  express from 'express';
 import bodyParser from 'body-parser';
 import playlistRouter from './routes/playlistRouter.js';
+import cors from 'cors';
 const app = express()
-const port = 3000
+const port = 3011
 
 app.use(bodyParser.json());
+app.options("*", cors({ origin: 'http://localhost:5173', optionsSuccessStatus: 200 }));
+app.use(cors({ origin: "http://localhost:5173", optionsSuccessStatus: 200 }));
 app.use('/api/playlist', playlistRouter);
 
 app.listen(port, () => {
